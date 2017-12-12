9b08a35690bb00caaa6d28f96cbcf29ec2abb79c
marcWong <yao.staufenberg@gmail.com>
2017-12-12 10:04:38 +0800

    add F1score,precision and recall statistics function

--- image/visualization.m
+++ image/visualization.m
@@ -1,31 +1,22 @@
 e = zeros([256 256 3]);
 ec = zeros([256 256 3]);
-a = imread('nms.jpg');
-b = imread('out249-gt.png');
-c = imread('hough.jpg');
+c = imread('nms-thres1.5.jpg');
+b = imread('out249-gt.jpg');
+a = imread('canny.jpg');
 
 for i = 1:256
     for j = 1:256
-        if c(i,j)>128;
-            ec(i,j,1) = 255;
-            ec(i,j,2) = 255;
-        end
-    end
-end
-
-for i = 1:256
-    for j = 1:256
-        if a(i,j)>128 && b(i,j,1)>128
+        if c(i,j)>128 && b(i,j)>128
             e(i,j,1) = 255;
-        elseif a(i,j)>128 && b(i,j,1)<= 128
+        elseif c(i,j)>128 && b(i,j)<= 128
             e(i,j,2) = 255;
-        elseif b(i,j,1)>128 && a(i,j)<= 128
+        elseif b(i,j)>128 && c(i,j)<= 128
             e(i,j,3) = 255;
         end
     end
 end
 
-e = (e + ec);
+%e = (e + ec);
 
 e=uint8(e);
-imwrite(e,'1.png');
+imwrite(e,'hed_nms.png');
