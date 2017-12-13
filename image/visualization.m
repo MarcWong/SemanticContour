aa494a334f0aea2909a0b28e34b99cc203c9ed61
marcWong <yao.staufenberg@gmail.com>
2017-12-14 00:08:53 +0800

    add batch process

--- image/visualization.m
+++ image/visualization.m
@@ -1,22 +1,32 @@
-e = zeros([256 256 3]);
-ec = zeros([256 256 3]);
-c = imread('nms-thres1.5.jpg');
-b = imread('out249-gt.jpg');
-a = imread('canny.jpg');
-
-for i = 1:256
-    for j = 1:256
-        if c(i,j)>128 && b(i,j)>128
-            e(i,j,1) = 255;
-        elseif c(i,j)>128 && b(i,j)<= 128
-            e(i,j,2) = 255;
-        elseif b(i,j)>128 && c(i,j)<= 128
-            e(i,j,3) = 255;
+fid = fopen('~/train_1.lst');
+path = 'visualization/';
+while ~feof(fid)
+    e = zeros([256 256 3]);
+    ec = zeros([256 256 3]);
+    file_name = fgetl(fid);
+    file_name = strrep(file_name,'train/aug_data/0.0_1_0/','');
+    file_name = strrep(file_name,'.jpg','');
+    c = imread(['train/' file_name '_fusion.jpg']);
+    b = imread(['train/' file_name '-gt.png']);
+    %a = imread('canny.jpg');
+    if length(size(b))==3
+        b = rgb2gray(b);
+    end
+    [m n]=size(b);
+    for i = 1:m
+        for j = 1:n
+            if c(i,j)>150 && b(i,j)>128
+                e(i,j,1) = 255;
+            elseif c(i,j)>150 && b(i,j)<= 128
+                e(i,j,2) = 255;
+            elseif b(i,j)>128 && c(i,j)<= 150
+                e(i,j,3) = 255;
+            end
         end
     end
+
+    %e = (e + ec);
+
+    e=uint8(e);
+    imwrite(e,[path file_name '_visualization.png']);
 end
-
-%e = (e + ec);
-
-e=uint8(e);
-imwrite(e,'hed_nms.png');
--- main_nms.m
+++ main_nms.m
@@ -2,42 +2,74 @@
 gray_threshold = 50;
 %k = 5;
 %nms_threshold = [0.8*k k];
+load_from_mat = 1;
 %%
-file_num = '249';
-origin = imread(['image/out' file_num '-origin.png']);
-gt = rgb2gray(imread(['image/out' file_num '-gt.png']));
-a1 = rgb2gray(imread(['image/out' file_num '-1.png']));
-a2 = rgb2gray(imread(['image/out' file_num '-2.png']));
-a3 = rgb2gray(imread(['image/out' file_num '-3.png']));
-a4 = rgb2gray(imread(['image/out' file_num '-4.png']));
-a5 = rgb2gray(imread(['image/out' file_num '-5.png']));
-a6 = rgb2gray(imread(['image/out' file_num '-fusion.png']));
+fid = fopen('~/train_1.lst');
+path = 'image/train/';
+while ~feof(fid)
+    file_name = fgetl(fid);
+    file_name = strrep(file_name,'train/aug_data/0.0_1_0/','');
+    file_name = strrep(file_name,'.jpg','');
+    origin = imread([path file_name '.jpg']);
+    gt = rgb2gray(imread([path file_name '-gt.png']));
 
-aa =uint8((uint16(a1) + uint16(a2) + uint16(a3) + uint16(a4) + uint16(a5)+uint16(a6))./6.0);%fusion image
+    if load_from_mat == 1
+        load([path file_name '.mat']);
+        a1 = double(FrameStack{1});
+        a2 = double(FrameStack{2});
+        a3 = double(FrameStack{3});
+        a4 = double(FrameStack{4});
+        a5 = double(FrameStack{5});
+        a6 = double(FrameStack{6});
+        aa =(a1 + a2 + a3 + a4 + a5 + a6)./6;%fusion image
+        aa = uint8(aa.*255);
+    else
+        a1 = imread([path file_name '-out1.png']);
+        a2 = imread([path file_name '-out2.png']);
+        a3 = imread([path file_name '-out3.png']);
+        a4 = imread([path file_name '-out4.png']);
+        a5 = imread([path file_name '-out5.png']);
+        a6 = imread([path file_name '-fuse.png']);
+        if length(size(a1))==3
+            a1 = rgb2gray(a1);
+            a2 = rgb2gray(a2);
+            a3 = rgb2gray(a3);
+            a4 = rgb2gray(a4);
+            a5 = rgb2gray(a5);
+            a6 = rgb2gray(a6);
+        end
+        aa =uint8((uint16(a1) + uint16(a2) + uint16(a3) + uint16(a4) + uint16(a5)+uint16(a6))./6.0);%fusion image
+    end
 
-for i = 1:256
-    for j = 1:256
-        if aa(i,j) < gray_threshold %threshold
-            aa(i,j) = 0;
+
+    [m n] = size(aa);
+
+    for i = 1:m
+        for j = 1:n
+            if aa(i,j) < gray_threshold %threshold
+                aa(i,j) = 0;
+            end
         end
     end
+    %imshow(aa);
+    imwrite(aa,[path file_name '_fusion.jpg']);
+    nms1 = nms(a1);
+    nms2 = nms(a2);
+    nms3 = nms(a3);
+    nms4 = nms(a4);
+    nms5 = nms(a5);
+    nms_fusion = nms(aa);
+
+    %{ visualization
+    figure;
+    subplot(221);
+    imshow(origin);
+    subplot(222);
+    imshow(gt);
+    subplot(223);
+    imagesc(nms1),colormap(gray),axis image, axis off;
+    subplot(224);
+    imagesc(nms_fusion),colormap(gray),axis image, axis off;
+    imwrite(nms_fusion,[path file_name '_nms.jpg']);
+    %}
 end
-%imshow(aa);
-
-nms1 = nms(a1);
-nms2 = nms(a2);
-nms3 = nms(a3);
-nms4 = nms(a4);
-nms5 = nms(a5);
-nms_fusion = nms(aa);
-
-figure;
-subplot(221);
-imshow(origin);
-subplot(222);
-imshow(gt);
-subplot(223);
-imagesc(nms1),colormap(gray),axis image, axis off;
-subplot(224);
-imagesc(nms_fusion),colormap(gray),axis image, axis off;
-imwrite(nms_fusion,'nms-1.1.jpg');
